90eee0ea69a604c8a198f222954b8ca7e4c86024
Ravi Majithia <ravikiran.majithia@joubel.com>
2022-07-12 12:44:22 +0200

    JI-3714 Add accordion within standard page

--- standard-page.js
+++ standard-page.js
@@ -165,6 +165,7 @@ H5P.StandardPage = (function ($, EventDispatcher) {
    */
   StandardPage.prototype.focus = function () {
     this.$pageTitle.focus();
+    this.resize();
   };
 
   /**
@@ -260,6 +261,14 @@ H5P.StandardPage = (function ($, EventDispatcher) {
     return {
       childrenStates: childrenStates
     };
+  };
+
+
+  /**
+   * Resize function for responsiveness.
+   */
+  StandardPage.prototype.resize = function () {
+    this.$inner.css('height', this.$inner.get(0).getBoundingClientRect().height);
   };
 
   return StandardPage;
